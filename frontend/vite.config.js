8f48a4bbd34012d07fd67f159acd5ccb01390d5f
dsandovalflavio <flaviocesarsandoval@gmail.com>
2026-02-19 14:47:05 -0600

    update fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../src/sql_dag_flow/static',
+    emptyOutDir: true,
+  }
 })
